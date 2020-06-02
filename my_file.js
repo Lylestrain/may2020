248575a08dfd4e7b1b9585810790f12e7b96d5a7
Lyle Strain <lylestrain@gmail.com>
2020-06-02 19:30:23 -0400

    Update my_file.js

--- my_file.js
+++ my_file.js
@@ -1,5 +1,14 @@
 let x = 'testing'
 
- alert('first')
+ //alert('first')
 
- alert(x);
+ //alert(x);
+
+ //console.log('hello to all');
+
+ const my_element = document.createElement('h1');
+ let my_span = document.createElement('span');
+ my_span.innerText = 'New JavaScript Heading';
+ my_element.appendChild(my_span);
+ document.body.appendChild(my_element);
+bear.textContent = 'growl'
