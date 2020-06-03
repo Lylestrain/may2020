91ba488957a11ec2a2133dc2ece1f55555377f9c
Lyle Strain <lylestrain@gmail.com>
2020-06-03 00:41:32 -0400

    Update my_file.js

--- my_file.js
+++ my_file.js
@@ -11,4 +11,39 @@ let x = 'testing'
  my_span.innerText = 'New JavaScript Heading';
  my_element.appendChild(my_span);
  document.body.appendChild(my_element);
-bear.textContent = 'growl'
+
+ const subtext2 = "a client-side langauge interpreted by the browser.";
+
+ function newsubtext(params) {
+    // var subtext2 = "a client-side langauge interpreted by the browser.";
+     document.getElementById('subtitle').innerText = subtext2;
+ }
+
+
+ const theParent = document.querySelector("#theDude");
+ theParent.addEventListener("click", doSomething, false);
+
+ function doSomething(e) {
+     if(e.target !== e.currentTarget) {
+         let clickedItem = e.target.id;
+         alert("Hello from " + clickedItem);
+     }
+
+     e.stopPropagation();
+ }
+
+
+ function addBox() {
+     var temp = document.getElementById("temp").content;
+     var copy = document.importNode(temp, true);
+     document.getElementById("app").appendChild(copy);
+ }
+
+ function deleteBox(e) {
+     if(e.target.classList.contains("deleteButton")) {
+         e.target.parentElement.remove();
+     }
+ }
+ document.getElementById("app").addEventListener("click", deleteBox);
+
+ document.getElementById("btn").addEventListener("click", addBox);
